b8faf43e16aeed015eb46c802ea1ef81447a49ba
Andrey Petrenko <seriousdron@gmail.com>
2015-12-07 18:45:44 +0300

    Db quesries fixes/optimizing

--- code/web/stats.php
+++ code/web/stats.php
@@ -7,7 +7,7 @@ $t15m_ago = new DateTime("15 minutes ago");
 $s = $t15m_ago->format("Y-m-d H:i:s");
 $result = mysql_query("SELECT count(*) from mo where created_at > '$s'");
 $response['last_15_min_mo_count'] = current(mysql_fetch_row($result));
-$result = mysql_query("SELECT min(created_at), max(created_at) from mo order by id DESC limit 10000");
+$result = mysql_query("SELECT min(created_at), max(created_at) FROM (SELECT * FROM `mo` order by id DESC LIMIT 10000) `last`");
 $response['time_span_last_10k'] = mysql_fetch_row($result);
 
 echo json_encode($response)."\n";
